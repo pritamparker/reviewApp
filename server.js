5fc5c585a27b45a59043123d6e7e4e42a21ec294
pritamparker <pritamparker@gmail.com>
2017-04-09 21:19:55 +0530

    project structure

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require('http');
-http.createServer(function(req,res){
-	res.writeHead(200,{"Content-Type":"text/plain"});
-	res.write("Hello World");
-	res.end();
-}).listen(8000);
-console.log('Server started om localhost:8000');
+var url  = require('url');
+function startServer(route){
+	http.createServer(function(req,res){
+		var pathname = url.parse(req.url).pathname;
+		console.log('request recieved for'+pathname);
+		route(pathname);
+		res.writeHead(200,{"Content-Type":"text/plain"});
+		res.write("Hello World");
+		res.end();
+	}).listen(8000);
+	console.log('Server started om localhost:8000');
+}
+
+exports.startServer = startServer;
